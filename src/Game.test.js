bd9b0148aae6e9638a6efda89ade920955659b05
annika <annikaslund@gmail.com>
2019-03-22 10:49:39 -0700

    Add test for die lock

--- src/Game.test.js
+++ src/Game.test.js
@@ -5,5 +5,17 @@ import Game from "./Game";
 
 // smoke test
 it("renders without crashing", function () {
-mount(<Game />);
+    mount(<Game />);
+});
+
+let wrapper;
+
+beforeEach(function(){
+    wrapper = mount(<Game />);
+})
+
+it("toggles lock when die is clicked", function () {
+    let die = wrapper.find(".Die").first();
+    die.simulate("click");
+    expect(die.props().locked).toEqual(true);
 });
