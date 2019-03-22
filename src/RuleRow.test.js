0f44f3e20353edcc906328146b8b1da3d5b6e624
annika <annikaslund@gmail.com>
2019-03-22 16:58:06 -0700

    add tests

--- src/RuleRow.test.js
+++ src/RuleRow.test.js
@@ -11,8 +11,18 @@ it("renders without crashing", function () {
 // snapshot test
 it("matches snapshot", function () {
     let wrapper = mount(<RuleRow name="Ones" score={3}/>);
+    console.log(wrapper.html())
     let serialized = toJson(wrapper);
     expect(serialized).toMatchSnapshot();
 });
 
 // doScore passed in that runs when clicked - mock
+it("runs correct doScore on click", function(){
+    const scoreFn = jest.fn();
+    let wrapper = mount(<RuleRow name="Ones" score={3} doScore={ scoreFn }/>);
+    let ruleBtn = wrapper.find(".RuleRow").first();
+
+    ruleBtn.simulate("click");
+    expect(scoreFn).toHaveBeenCalled();
+    expect(wrapper.matchesElement(<tr class="RuleRow RuleRow-active"><td class="RuleRow-name">Ones</td><td class="RuleRow-score">3</td></tr>));
+});
