c43392e6cb4ebb02ca93be12a30d4a4c0625ff22
annika <annikaslund@gmail.com>
2019-03-22 11:26:11 -0700

    Fox bug, cannot keep rolling when out of rolls

--- src/Game.js
+++ src/Game.js
@@ -47,14 +47,18 @@ class Game extends Component {
   }
 
   toggleLocked(idx) {
+    // if roll count is 0 
+    // exit
+    if (this.state.rollsLeft > 0){
     // toggle whether idx is in locked or not
-    this.setState(st => ({
-      locked: [
-        ...st.locked.slice(0, idx),
-        !st.locked[idx],
-        ...st.locked.slice(idx + 1)
-      ],
-    }))
+      this.setState(st => ({
+        locked: [
+          ...st.locked.slice(0, idx),
+          !st.locked[idx],
+          ...st.locked.slice(idx + 1)
+        ],
+      }))
+    } 
   }
 
   doScore(rulename, ruleFn) {
