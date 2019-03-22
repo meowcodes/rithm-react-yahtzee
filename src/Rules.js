4769c195c508b7d38d7e68b1e065f50333834b5b
annika <annikaslund@gmail.com>
2019-03-22 11:21:38 -0700

    Rules SmallStraight functionality

--- src/Rules.js
+++ src/Rules.js
@@ -62,14 +62,29 @@ class SumDistro extends Rule {
 class FullHouse extends Rule {
   evalRoll = (dice) => {
     // do any of the counts meet of exceed this distro?
-    return ((this.freq(dice).includes(2) && this.freq(dice).includes(3)) ? 25 : 0);
+    return ((this.freq(dice).includes(2) && this.freq(dice).includes(3)) ? this.score : 0);
   }
 }
 
 /** Check for small straights. */
 
-class SmallStraight {
-  // TODO
+class SmallStraight extends Rule {
+  evalRoll = (dice) => {
+    const d = new Set(dice);
+
+    // large straight must be 5 different dice & only one can be a 1 or a 6
+    if (d.size >= 4 && d.has(3) && d.has(4)) {
+      if (d.has(1) && d.has(2)){
+        return this.score;
+      } else if (d.has(2) && d.has(5)){
+        return this.score;
+      } else if (d.has(5) && d.has(6)){
+        return this.score;
+      }
+    }
+
+    return 0;
+  }
 }
 
 /** Check for large straights. */
@@ -105,10 +120,10 @@ const threeOfKind = new SumDistro({ count: 3 });
 const fourOfKind = new SumDistro({ count: 4 });
 
 // full house scores as flat 25
-const fullHouse = new FullHouse();
+const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
+const smallStraight = new SmallStraight({ score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
